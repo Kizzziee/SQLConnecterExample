1156208f1a774d39f599b6d935bb96038d16543e
2-KAbban <2-KAbban@BH-R20-31.local.gfmat.org>
2023-03-30 14:22:43 +0100

    30/03/2023- This program is from lesson where we first connected to Github.

---
+++ src/main/java/com/mycompany/sqlconnectorexample/DAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.sqlconnectorexample;
+
+/**
+ *
+ * @author 2-KAbban
+ */
+public class DAO {
+    public static void createTable(){
+        System.out.println("Hello world!");
+    }
+}
